00026228dffc8d8bf076c84fbb3f27eb08646bc5
KoloinaAndri <107675357+KoloinaAndri@users.noreply.github.com>
2022-12-16 13:32:50 +0100

    Update Livre.py

--- Livre.py
+++ Livre.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
-
 import os 
+import shutil
 from zipfile import ZipFile 
 from PyPDF2 import PdfReader, PdfFileReader
 import pandas as pd
 import pdfkit as pdf
 import fitz
-#from langdetect import detect
 import py3langid as langid
 import epub
 import eyed3
 import numpy as np 
 from fpdf import FPDF
 import aspose.words as aw
-
+import time
 
 
 class Ressource():
@@ -150,21 +149,14 @@ class Livre():
         return self
 
     
-"""
-    def recup(self,path,filename):
-        path_3 = path + "/" + str(filename)
-        with open(path_3, "rb") as pdf_file:
-            read_pdf = PyPDF2.PdfFileReader(pdf_file, strict = False)
-            #page = read_pdf.pages[0]
-            #page_content = page.extract_text()
-        return read_pdf  
-"""
 class Bibliotheque():
 
     def __init__(self):
         self.path = os.getcwd() + "/livres"
         self.livres = []
         self.table_auteur = []
+
+
 
     def sommaire(self, livre):
         t = livre.table 
@@ -177,8 +169,13 @@ class Bibliotheque():
                 sommaire.append(G)
             else :
                 G += t[i]
+        if ".epub" in livre.filename:
+            nom_fichier = livre.filename[:-5]
+        else:
+            nom_fichier = livre.filename[:-4]
+
         sommaire2 = pd.DataFrame(sommaire, columns = ["sommaire"])
-        nom_sommaire = livre.filename + "_sommaire"
+        nom_sommaire = nom_fichier + "_sommaire"
         sommaire2.to_html(nom_sommaire + '.html', index=False)  #create a html_file just to visualize the table
         np.savetxt(nom_sommaire + '.txt', sommaire2,fmt='%s', encoding="utf-8") #save the dataFrame as a txt file
         pdf = FPDF()    #save FPDF() class into a variable pdf 
@@ -190,8 +187,27 @@ class Bibliotheque():
         pdf.output(nom_sommaire + ".pdf") #save txt_file as  a pdf_file
         doc = aw.Document(nom_sommaire + ".txt")
         doc.save(nom_sommaire + ".epub") #save txt_file as an epub_file
-        
-                
+        f.close()
+
+        sommaires = [nom_sommaire + ".pdf",nom_sommaire + ".epub",  nom_sommaire + '.html',nom_sommaire + ".txt" ]
+
+        for sommaire in sommaires:
+            shutil.move(os.getcwd() + "/" + sommaire, os.getcwd() + "/les_sommaires" + "/"+ sommaire)
+
+
+
+
+    def sup_sommaire(self,nom_fichier):
+        if ".epub" in nom_fichier:
+            nom_sommaire = nom_fichier[:-5] +"_sommaire"
+        else:
+            nom_sommaire = nom_fichier[:-4] +"_sommaire"
+
+        os.remove(os.getcwd() + "/les_sommaires/" + nom_sommaire + ".pdf")
+        os.remove(os.getcwd() + "/les_sommaires/" + nom_sommaire + ".epub")
+        os.remove(os.getcwd() + "/les_sommaires/" + nom_sommaire + ".txt")
+        os.remove(os.getcwd() + "/les_sommaires/" + nom_sommaire + ".html")
+
     
     def recup(self,path = None,el = None):
         if path:
@@ -200,34 +216,72 @@ class Bibliotheque():
                 new_path += '/' + el
         else:
             new_path = self.path
+            os.mkdir(self.path[:-7]   +'/les_sommaires')
+            os.mkdir(self.path[:-7]  + "/liste_auteurs")
+            os.mkdir(self.path[:-7]  + "/liste_ouvrages")
+            file = open(os.getcwd()+"/log.txt", "w") 
+            file.write("Fichier Log:" +"\n") 
+            file.close()
+            file = open(os.getcwd()+"/bibli2.conf", "w")
+            file.write("Fichier de Configuration :" +"\n") 
+            file.close()
+            file = open(os.getcwd()+"/bibli2.conf", "a")            
+            dossier = os.listdir(os.getcwd())
+            for i in range(len(dossier)):
+                file.write(dossier[i]+"\n") 
+            file.close()
 
+
+        
         bibli = os.listdir(new_path) #list all of the files/directory in the new_path created
         err = ["verne_phare_bout_monde_mv.pdf","verne_phare_bout_monde.pdf","verne_geographie_france_et_colonies_ocr.pdf","autorun.pdf","pochette_cd_conan_doyle.pdf","epinal"]
         for i in range(len(bibli)):
             element = bibli[i]
             print(element)
             if not "." in element and not element in err:
+                file = open(os.getcwd()+"/log.txt", "a") 
+                file.write("Traitement du dossier : "+ element +"\n") 
+                file.close()
                 path_2 = new_path +"/"+ element
                 Bibliotheque.recup(self,path_2)
             elif not element in err:
                 B = Livre(new_path)
-
                 livre = B.recup(element)
+                file = open(os.getcwd()+"/log.txt", "a") 
+                file.write("Ajout du fichier : "+ element +"\n") 
+                file.close()
                 if not livre.author in [row.author for row in self.livres] or not livre.title in [row.title for row in self.livres]: 
                     self.livres.append(livre)
                     Bibliotheque.ajoute_auteur(self,livre)
                     Bibliotheque.sommaire(self,livre)
         return self.livres
 
-    def DataFrame(self):
+
+
+
+
+    def DataFrame(self,ouvrages = None, auteurs = None):
         #Construction de la data frame correspondant au liste des ouvrages
-        C_aut = pd.DataFrame([row.author + " | " for row in self.livres],columns = ["auteurs"])
-        C_liv = pd.DataFrame([row.title + " | "  for row in self.livres],columns = ["livres"])
-        C_lang = pd.DataFrame([row.language + " | "  for row in self.livres],columns = ["langue"])
-        C_nom = pd.DataFrame([" > " + row.filename + " | " for row in self.livres],columns = ["nom du fichier"])
+        if ouvrages:
+            C_aut = pd.DataFrame([row[2] + " | " for row in ouvrages],columns = ["auteurs"])
+            C_liv = pd.DataFrame([row[1] + " | "  for row in ouvrages],columns = ["livres"])
+            C_lang = pd.DataFrame([row[3] + " | "  for row in ouvrages],columns = ["langue"])
+            C_nom = pd.DataFrame([" > " + row[0] + " | " for row in ouvrages],columns = ["nom du fichier"])
+            L1 = pd.DataFrame([" > " + row[0] + " : " for row in auteurs],columns = ["auteurs"])
+            L2 = pd.DataFrame([row[1] for row in auteurs],columns = ["livres et leur nom de fichier"])
+
+        else:
+            C_aut = pd.DataFrame([row.author + " | " for row in self.livres],columns = ["auteurs"])
+            C_liv = pd.DataFrame([row.title + " | "  for row in self.livres],columns = ["livres"])
+            C_lang = pd.DataFrame([row.language + " | "  for row in self.livres],columns = ["langue"])
+            C_nom = pd.DataFrame([" > " + row.filename + " | " for row in self.livres],columns = ["nom du fichier"])
+            L1 = pd.DataFrame([" > " + row[0] + " : " for row in self.table_auteur],columns = ["auteurs"])
+            L2 = pd.DataFrame([row[1] for row in self.table_auteur],columns = ["livres et leur nom de fichier"])
+
+        
         DataFrame_auteurs = pd.concat([C_nom,C_liv,C_aut,C_lang],axis = 1) #create a dataframe of all of the books
         DataFrame_auteurs.to_html('Ouvrages.html', index=False)  #create a html_file just to visualize the table
-        np.savetxt('Ouvrages.txt', DataFrame_auteurs,fmt='%s') #save the dataFrame as a txt file
+        np.savetxt('Ouvrages.txt', DataFrame_auteurs,fmt='%s',encoding="utf-8") #save the dataFrame as a txt file
         pdf = FPDF()    #save FPDF() class into a variable pdf 
         pdf.add_page()  #add a page
         pdf.set_font("Helvetica", size = 8)
@@ -237,13 +291,18 @@ class Bibliotheque():
         pdf.output("Ouvrages.pdf") #save txt_file as  a pdf_file
         doc = aw.Document("Ouvrages.txt")
         doc.save("Ouvrages.epub") #save txt_file as an epub_file
+        f.close()
+
+        ouvrages = [ 'Ouvrages.html', 'Ouvrages.txt' , "Ouvrages.pdf", "Ouvrages.epub"]
+
+        for ouvrage in ouvrages:
+            shutil.move(os.getcwd() + "/" + ouvrage, os.getcwd() + "/liste_ouvrages" + "/"+ ouvrage)
+                
 
         #Construction de la data frame correspondant au liste des auteurs avec leurs oeuvres
-        L1 = pd.DataFrame([" > " + row[0] + " : " for row in self.table_auteur],columns = ["auteurs"])
-        L2 = pd.DataFrame([row[1] for row in self.table_auteur],columns = ["livres et leur nom de fichier"])
         L = pd.concat([L1,L2],axis = 1) #create a dataframe of all of the authors
         L.to_html('Auteurs.html', index=False) #create a html_file just to visualize the table
-        np.savetxt('Auteurs.txt', L,  fmt='%s') #save the DataFrame as a txt_file
+        np.savetxt('Auteurs.txt', L,  fmt='%s',encoding="utf-8") #save the DataFrame as a txt_file
         pdf = FPDF() # save FPDF() class into a variable pdf    
         pdf.add_page() # Add a page
         pdf.set_font("Arial", size = 8) # set style and size of font
@@ -253,42 +312,14 @@ class Bibliotheque():
         pdf.output("Auteurs.pdf") #save txt_file as a pdf_file
         doc = aw.Document("Auteurs.txt")
         doc.save("Auteurs.epub") #save txt_file as an epub_file
+        f.close()
 
-    def clear(self):
-        self.livres = []
-        self.table_auteur = []
+        listes_auteur = [ 'Auteurs.html', 'Auteurs.txt',"Auteurs.pdf", "Auteurs.epub"]
 
-    def __isub__(self,element):
-        if isinstance(element,Livre):
-            self.livres.remove(element)
-
-
-
-    def __iadd__(self,element):
-        if isinstance(element,Livre):
-            if not element.author in [row.author for row in self.livres] or not element.title in [row.title for row in self.livres]:
-                self.livres.append(element)
-                Bibliotheque.ajoute_auteur(element)
-
-        elif isinstance(element,str):
-            if ".conf" in element:
-                Bibliotheque.recup(self.path,element)                
-                A = Livre.recup(element)
-                if not A.author in [row.author for row in self.livres] or not A.title in [row.title for row in self.livres]: 
-                    self.livres.append(A)
-                    Bibliotheque.ajoute_auteur(A)
-            else :
-                A = Livre.recup(element)
-                if not A.author in [row.author for row in self.livres] or not A.title in [row.title for row in self.livres]: 
-                    self.livres.append(A)
-                    Bibliotheque.ajoute_auteur(A)
-
-        elif isinstance(objet, Bibliotheque):
-            for i in range(len(objet)):
-                if not B[i].auteur in [row.author for row in self.livres] or not B[i].title in [row.title for row in self.livres]:
-                    self.livres.append(B[i])
-                    Bibliotheque.ajoute_auteur(B[i])
+        for liste_auteur in listes_auteur:
+            shutil.move(os.getcwd() + "/" + liste_auteur, os.getcwd() + "/liste_auteurs" + "/"+ liste_auteur)
         
+
 
     def ajoute_auteur(self,element):
         j = -1
@@ -296,117 +327,112 @@ class Bibliotheque():
             if self.table_auteur[i][0] == element.author:
                 j = i
         if j == -1 :
-            self.table_auteur.append([element.author, "\n" + str([element.title,element.filename]) + "\n" ])
+            self.table_auteur.append([element.author, "\n" + str([element.title,element.filename+ " "]) + "\n" ])
         else:
-            self.table_auteur[j][1] += str([element.title,element.filename]) + "\n"
+            self.table_auteur[j][1] += str([element.title,element.filename+ " "]) + "\n"
 
 
-    def ajouter_dans_auteur(self,element):
-        myfile = open(os.getcwd() + "/Auteurs.txt", "rt") 
-        texte_auteur = myfile.read() 
-        if not element.author in texte_auteur:
-            texte_auteur +=  element.author +'\n' +'[' + element.title+ ', ' + element.filename +' ]'
-        
-        else:
-            nom_auteur =""
-            for i in range(len(texte_auteur)):                
-                nom_auteur += texte_auteur[i]
-                if texte_auteur[i] == '[' and texte_auteur[i-3] != ']':
-                    if nom_auteur == element.author:
-                        temp = texte_auteur.split()
-                        mid_pos = i-1
-                        texte_auteur = ' '.join(temp[:mid_pos] + ['[' + element.title+ ', ' + element.filename +' ]'] + temp[mid_pos:])
-                        break
-                    nom_auteur = ""
-                if texte_auteur[i] != ']':
-                    nom_auteur = ""
+
 
 
     def update(self):
-        myfile = open(os.getcwd() + "/Ouvrages.txt", "rt") 
-        texte_ouvrage = myfile.read() 
-        bibli = os.listdir(os.getcwd() + "/livres2")
-        print(bibli)
+        myfile1 = open(os.getcwd() + "/liste_ouvrages/Ouvrages.txt", "r",encoding = "utf-8") 
+        texte_ouvrage = myfile1.read() 
+        myfile2 = open(os.getcwd() + "/liste_auteurs/Auteurs.txt", "r",encoding = "utf-8")
+        texte_auteur = myfile2.read()
+        bibli = os.listdir(os.getcwd() + "/livres")[20:150]
+        Liste_ouvrages = []
+        Liste_auteurs = []
+        err = ["verne_phare_bout_monde_mv.pdf","verne_phare_bout_monde.pdf","verne_geographie_france_et_colonies_ocr.pdf","autorun.pdf","pochette_cd_conan_doyle.pdf"]
         for i in range(len(bibli)):
-            livre = bibli[i]
-            if not livre in texte_ouvrage:
+            livre = bibli[i]        
+            print(livre)   
+            if not livre in Liste_ouvrages and ".zip" not in livre and "." in livre and livre not in err: 
                 A = Livre(os.getcwd() + "/livres")
                 element = A.recup(livre)
-                texte_ouvrage += element.filename+' | ' + element.title+' | ' + element.author+' | ' + element.language+' | '
-                Bibliotheque.ajouter_dans_auteur(self,element) 
-            else:
-                k = 0
-                trouve_livre = False
-                nom_auteur = ""
-                nom_livre = ""
-                nom_fichier = ""
-                nom_langue = ""
-                for i in range(len(texte_ouvrage)):
-                    if k%4 == 0:
-                        nom_fichier +=  texte_ouvrage[i]
-                        if trouve_livre:
+                Liste_ouvrages.append([ element.filename, element.title, element.author,element.language])
+                nom_auteurs = [row[0] for row in Liste_auteurs]
+                if not element.author in nom_auteurs:
+                    Bibliotheque.sommaire(self,element)
+                    Liste_auteurs.append([element.author, "\n" + str([element.title,element.filename+ " "]) + "\n" ])
+                else:
+                    for j in range(len(Liste_auteurs)):
+                        if nom_auteurs[j] == element.author:
+                            Liste_auteurs[j][1] += str([element.title,element.filename+ " "]) + "\n"
                             break
-
-                    if texte_ouvrage[i] == '|':
-                        k += 1
-                        if k == 1:
-                            nom_fichier = nom_fichier[:len(nom_fichier)-2]
-                            if nom_fichier == livre:
-                                trouve_livre = True
-                            else:
-                                nom_fichier =""
-                        else: 
-                            nom_fichier = nom_fichier[:len(nom_fichier)-2]
-                            if nom_fichier == livre:
-                                trouve_livre = True
-                            else:
-                                nom_fichier =""
-                    if trouve_livre:
-                        if k%4 == 1:
-                            nom_livre  += texte_ouvrage[i]
-                        elif k%4 == 2:
-                            nom_auteur  += texte_ouvrage[i]
-                        elif k%4 == 3:
-                            nom_langue  += texte_ouvrage[i]                        
-                nom_livre = nom_livre[:len(nom_fichier)-2]
-                nom_auteur = nom_auteur[:len(nom_fichier)-2]
-                nom_langue = nom_langue[:len(nom_fichier)-2] +"\n"
-   
+                if livre not in texte_ouvrage:
+                        file = open(os.getcwd()+"/log.txt", "a") 
+                        file.write("Ajout du fichier : " + livre +"\n") 
+                        file.close()
         k = 0
-        nom_auteur = ""
-        nom_livre = ""
         nom_fichier = ""
-        nom_langue = ""
         i = 0
-        element_a_enlever = False
+        element_a_ajouter = False
         mot = texte_ouvrage.split()
-        while ( i != len(mot) ):
-            if k%4 == 0 and mot[i] != "|":
+        while ( i != len(mot)):
+            if k%4 == 0 and mot[i] != ">" and mot[i] != "|":
                 nom_fichier += mot[i]
             if mot[i] == '|':
                 k +=1
                 if k%4 == 1:
                     if not nom_fichier in bibli:
-                        element_a_enlever = True
-                        debut = i- 2
+                        element_a_ajouter = False
+                        Bibliotheque.sup_sommaire(self,nom_fichier)
+                        file = open(os.getcwd()+"/log.txt", "a") 
+                        file.write("Suppression du fichier : " +nom_fichier +"\n") 
+                        file.close()
                     else:
-                        element_a_enlever = False
+                        element_a_ajouter = True
+                        if not livre in Liste_ouvrages: 
+                            A = Livre(os.getcwd() + "/livres")
+                            element = A.recup(livre)
+                            Liste_ouvrages.append([ element.filename, element.title, element.author,element.language])
+                            nom_auteurs = [row[0] for row in Liste_auteurs]
+                            if not element.author in nom_auteurs:
+                                Bibliotheque.sommaire(self,element)
+                                Liste_auteurs.append([element.author, "\n" + str([element.title,element.filename+ " "]) + "\n" ])
+                            else:
+                                for j in range(len(Liste_auteurs)):
+                                    if nom_auteurs[j] == element.author:
+                                        Liste_auteurs[j][1] += str([element.title,element.filename+ " "]) + "\n"
+                                        break
                     nom_fichier = ""
-                elif k%4 == 0 and element_a_enlever == True:
-                    fin = i
-                    mot = mot[:debut] + mot[fin:]
-                    i = debut
-
             i+=1
-        texte_ouvrage = ' '.join(mot)
+        myfile1.close()
+        myfile2.close()
+        Bibliotheque.DataFrame(self,Liste_ouvrages,Liste_auteurs) 
 
-        print(texte_ouvrage)
 
 
+    def update_bibli(self):
+        dossier = os.listdir(os.getcwd())
+        print(dossier)
+        file = open(os.getcwd()+"/bibli2.conf", "r")
+        liste_conf = file.readlines()
+        file.close()
+        liste_finale = liste_conf[0]
+        for i in range(len(dossier)):
+            dossier[i] += "\n" 
 
-    
-B = Bibliotheque()
-B.recup()
-B.DataFrame()
+        for i in range(1,len(liste_conf)):
+            if liste_conf[i] in dossier:
+                liste_finale+= liste_conf[i]
+            else:
+                file = open(os.getcwd()+"/log.txt", "a") 
+                file.write("Suppression du fichier/dossier : " +liste_conf[i]) 
+                file.close()
+        
+
+        for i in range(len(dossier)):
+            if not dossier[i] in liste_conf:
+                liste_finale += dossier[i]
+                file = open(os.getcwd()+"/log.txt", "a") 
+                file.write("Ajout du fichier/dossier : " +dossier[i]) 
+                file.close()
+
+        file = open(os.getcwd()+"/bibli2.conf", "w")
+        file.write(liste_finale) 
+        file.close()
+
 
 
